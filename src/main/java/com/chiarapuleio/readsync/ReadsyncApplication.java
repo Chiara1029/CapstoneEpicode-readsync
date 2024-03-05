1582cc91bcd98980f7ea6838ef3ce283bc12ac2d
Chiara Puleio <chiarapuleio@yahoo.it>
2024-03-06 00:35:45 +0100

    Initial commit, empty packages

---
+++ src/main/java/com/chiarapuleio/readsync/ReadsyncApplication.java
@@ -0,0 +1,13 @@
+package com.chiarapuleio.readsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chiarapuleio/readsync/ReadsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiarapuleio.readsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
