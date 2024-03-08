a23b48884f426a8f31375c0187132af56879397a
Chiara Puleio <chiarapuleio@yahoo.it>
2024-03-08 16:21:09 +0100

    exceptions created to handle user's requests

---
+++ src/main/java/com/chiarapuleio/readsync/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.chiarapuleio.readsync.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
